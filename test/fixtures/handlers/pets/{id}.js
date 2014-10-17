de8ca3aa1e82fba76a796bc414a42d6a481ad877
Trevor Livingston <trlivingston@paypal.com>
2014-10-17 13:44:31 -0500

    testing out pre handlers

--- test/fixtures/handlers/pets/{id}.js
+++ test/fixtures/handlers/pets/{id}.js
@@ -3,9 +3,14 @@
 var store = require('../../lib/store');
 
 module.exports = {
-    get: function (req, reply) {
-        reply(store.get(req.params.id));
-    },
+    get: [
+        function p1(req, reply) {
+            reply(store.get(req.params.id));
+        },
+        function handler(req, reply) {
+            reply(req.pre.p1);
+        }
+    ],
     delete: function (req, reply) {
         store.delete(req.params.id);
         reply(store.all());
