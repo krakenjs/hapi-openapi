a38737a8a607d98feecd348057bc233a499c8f24
Trevor Livingston <trlivingston@paypal.com>
2015-02-09 12:16:17 -0600

    removed commented out test values

--- test/test-swaggerize-hapi.js
+++ test/test-swaggerize-hapi.js
@@ -119,10 +119,8 @@ Test('test', function (t) {
         var queryStringToStatusCode = {
             'limit=2': 200,
             'tags=some_tag&tags=some_other_tag': 200,
-            //'tags=billy,bob': 200,
             'limit=2&tags=some_tag&tags=some_other_tag': 200,
-            'limit=a_string': 400,
-            //'unspecified_parameter=value': 200
+            'limit=a_string': 400
         }
 
         t.plan(Object.keys(queryStringToStatusCode).length);
