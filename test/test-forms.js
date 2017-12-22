85964f70011ce699f9f267c4db356242891f754d
Trevor Livingston <tlivings@gmail.com>
2017-12-22 10:45:14 -0600

    fixed form validation

--- test/test-forms.js
+++ test/test-forms.js
@@ -5,10 +5,11 @@ const Path = require('path');
 const Swaggerize = require('../lib');
 const Hapi = require('hapi');
 
+
 Test('form data', function (t) {
 
     t.test('upload', async function (t) {
-        t.plan(2);
+        t.plan(1);
 
         try {
             const server = new Hapi.Server();
@@ -20,22 +21,12 @@ Test('form data', function (t) {
                     handlers: {
                         upload: {
                             post: function (req, h) {
-                                t.strictEqual(typeof req.payload, 'object', 'read payload.');
                                 return '';
                             }
                         }
                     }
                 }
             });
-
-            // server.route({
-            //     method: 'POST',
-            //     path: '/v1/petstore/upload',
-            //     handler: function (req, h) {
-            //         t.strictEqual(typeof req.payload, 'object', 'read payload.');
-            //         return '';
-            //     }
-            // })
 
             const response = await server.inject({
                 method: 'POST',
@@ -49,35 +40,81 @@ Test('form data', function (t) {
             t.strictEqual(response.statusCode, 200, `${response.request.path} OK.`);
         }
         catch (error) {
-            //t.fail(error.message);
+            t.fail(error.message);
         }
 
     });
 
-    // t.test('bad content type', function (t) {
-    //     t.plan(1);
-    //
-    //     server.inject({
-    //         method: 'POST',
-    //         url: '/v1/petstore/upload',
-    //         payload: 'name=thing&upload=data'
-    //     }, function (response) {
-    //         t.strictEqual(response.statusCode, 400, '400 status.');
-    //     });
-    // });
-    //
-    // t.test('invalid payload', function (t) {
-    //     t.plan(1);
-    //
-    //     server.inject({
-    //         method: 'POST',
-    //         url: '/v1/petstore/upload',
-    //         headers: {
-    //             'content-type': 'application/x-www-form-urlencoded'
-    //         },
-    //         payload: 'name=thing&upload='
-    //     }, function (response) {
-    //         t.strictEqual(response.statusCode, 400, '400 status.');
-    //     });
-    // });
+    t.test('bad content type', async function (t) {
+        t.plan(1);
+
+        try {
+            const server = new Hapi.Server();
+
+            await server.register({
+                plugin: Swaggerize,
+                options: {
+                    api: Path.join(__dirname, './fixtures/defs/pets.json'),
+                    handlers: {
+                        upload: {
+                            post: function (req, h) {
+                                return '';
+                            }
+                        }
+                    }
+                }
+            });
+
+            const response = await server.inject({
+                method: 'POST',
+                url: '/v1/petstore/upload',
+                payload: 'name=thing&upload=data'
+            });
+
+            t.strictEqual(response.statusCode, 415, `${response.request.path} unsupported media type.`);
+        }
+        catch (error) {
+            t.fail(error.message);
+        }
+
+    });
+
+
+    t.test('invalid payload', async function (t) {
+        t.plan(1);
+
+        try {
+            const server = new Hapi.Server();
+
+            await server.register({
+                plugin: Swaggerize,
+                options: {
+                    api: Path.join(__dirname, './fixtures/defs/pets.json'),
+                    handlers: {
+                        upload: {
+                            post: function (req, h) {
+                                return '';
+                            }
+                        }
+                    }
+                }
+            });
+
+            const response = await server.inject({
+                method: 'POST',
+                url: '/v1/petstore/upload',
+                headers: {
+                    'content-type': 'application/x-www-form-urlencoded'
+                },
+                payload: 'name=thing&upload='
+            });
+
+            t.strictEqual(response.statusCode, 400, `${response.request.path} validation error.`);
+        }
+        catch (error) {
+            t.fail(error.message);
+        }
+
+    });
+
 });
